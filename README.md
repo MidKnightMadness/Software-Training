ebbadb398db59349be1188fa02acd44fe9f180ea
glingy <gregory.ling@students.tka.org>
2019-08-21 21:59:44 -0700

    Math and Variable lessons added.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Software Training
+Goals:
+ * Introduce members to Java - variables, math, methods, classes, DcMotor, Telemetry, and the FTC Docs
+ * Introduce members to GitHub - pull, commit, push, conflict cycle
+
+Notes:
+We need a challenge for each part - variables, math, methods... One file for the lesson (which I’ve made examples for variables and math) and one file that outlines a challenge for them to use their new knowledge. It should be straightforward to mess up (something easy to forget).
+
+Let me know if you’ve pulled and are working on it and push when you’re done so we don’t both work on the same lesson.
+
+[FTC Docs](insert url here)
+[GitHub Organization](https://github.com/MidKnightMadness)
---
+++ src/org/tka/robotics/Variables.java
@@ -0,0 +1,29 @@
+package org.tka.robotics;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int someInteger = 1;
+	    double someDouble = 5;
+
+	    System.out.println("The integer is " + someInteger + " and the double is " + someDouble + ".");
+    }
+}
+
+
+/*
+# Notes:
+
+Refactor
+   - right-click on the class in the tree view and use refactor to change the name of the class
+   - changes the name of the class, the file, and any references to the class in other files
+   OR
+   - right-click on a variable to change it and any other references to it
+
+Style Guide
+   - Classes - each word starts with a capital letter, ExampleClass
+   - properties, variables, and methods - camelCase - each word starts with a capital letter except the first, exampleVariable
+   - static properties - all caps with underscores for spaces, EXAMPLE_CONSTANT
+
+ */
